bad7156da0cd734590a5074415bb823bcb913bfb
Mihir Pathak <mihirpathak97@users.noreply.github.com>
2019-07-13 13:38:09 +0530

    Add custom 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,11 +3,17 @@ import React from "react"
 import Layout from "../components/layout/index";
 import SEO from "../components/seo";
 
+import {
+  Icon
+} from 'antd';
+
 const NotFoundPage = () => (
   <Layout>
     <SEO title="Error" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div className="error">
+      <Icon type="warning" style={{color: 'yellow', fontSize: '64px'}}></Icon>
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    </div>
   </Layout>
 )
 
