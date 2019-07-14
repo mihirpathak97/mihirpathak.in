69374595f6108a5c3074df0a11a23c8050721486
Mihir Pathak <mihirpathak97@users.noreply.github.com>
2019-07-14 17:56:43 +0530

    Add createPage logic and template

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,4 +4,50 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
+const path = require(`path`)
+
+/**
+ * Create "dynamic" pages for blog posts
+ */
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+
+  return new Promise((resolve, reject) => {
+    const blogPostTemplate = path.resolve(`src/templates/blog-post.jsx`)
+    // Query for markdown nodes to use in creating pages.
+    resolve(
+      graphql(`
+				query {
+					allContentfulBlogPost {
+						nodes {
+							title
+							slug
+							contentful_id
+							createdAt(formatString: "DD MMM YYYY")
+							content {
+								json
+							}
+						}
+					}
+				}
+			`
+      ).then(result => {
+        if (result.errors) {
+          reject(result.errors)
+        }
+
+        // Create pages for each blog post.
+        result.data.allContentfulBlogPost.nodes.forEach(post => {
+          const path = '/blog/' + post.slug
+          createPage({
+            path,
+            component: blogPostTemplate,
+            context: {
+              post
+            },
+          })
+        })
+      })
+    )
+  })
+}
