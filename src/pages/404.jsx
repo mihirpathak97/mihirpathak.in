201cccc52020aa3441a5881897d813e3cebfe04b
Mihir Pathak <mihirpathak97@users.noreply.github.com>
2019-07-14 17:17:42 +0530

    404: Use Link tag

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,4 +1,5 @@
-import React from "react"
+import React from "react";
+import { Link } from 'gatsby';
 
 import Layout from "../components/layout/index";
 import SEO from "../components/seo";
@@ -12,7 +13,7 @@ const NotFoundPage = () => (
     <SEO title="Error" />
     <div className="error">
       <Icon type="warning" style={{color: 'yellow', fontSize: '64px'}}></Icon>
-      <p>You just hit a route that doesn&#39;t exist... the sadness. Lets get you <a href="/">home</a></p>
+      <p>You just hit a route that doesn't exist... the sadness. Lets get you <Link className="link" to="/">home</Link></p>
     </div>
   </Layout>
 )
