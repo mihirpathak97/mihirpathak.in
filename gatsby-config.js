5f24a9c9daafaa0717277f3121b7a11e88331afd
Mihir Pathak <mihirpathak97@users.noreply.github.com>
2019-07-14 20:37:49 +0530

    Add plugin to remove service worker

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,5 +40,7 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    // Remove service worker
+    `gatsby-plugin-remove-serviceworker`
   ],
 }
