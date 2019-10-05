65d96d72688811832047fd14418483fbd0c3e84f
Mihir Pathak <mihirpathak97@users.noreply.github.com>
2019-10-05 16:34:05 +0530

    Simply site

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -1,51 +1,66 @@
 import React, { useState } from 'react'
 import PropTypes from 'prop-types'
-import { useStaticQuery, graphql, Link } from 'gatsby'
+import { useStaticQuery, graphql } from 'gatsby'
 import Img from 'gatsby-image'
 
 import { Icon, Drawer } from 'antd'
 
-const Layout = ({ location, children }) => {
+const NavbarSvg = () => (
+  <svg
+    viewBox="64 64 896 896"
+    data-icon="menu"
+    width="1em"
+    height="1em"
+    fill="currentColor"
+    aria-hidden="true"
+    focusable="false"
+  >
+    <path d="M904 160H120c-4.4 0-8 3.6-8 8v64c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-64c0-4.4-3.6-8-8-8zm0 624H120c-4.4 0-8 3.6-8 8v64c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-64c0-4.4-3.6-8-8-8zm0-312H120c-4.4 0-8 3.6-8 8v64c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-64c0-4.4-3.6-8-8-8z"></path>
+  </svg>
+)
+
+const sidebarContent = (data) => (
+  <>
+    <Img
+      className="profile"
+      fluid={data.contentfulInfo.profileImage.fluid}
+    />
+    <div className="title">
+      <h1 className="name">{data.contentfulInfo.name}</h1>
+      <h2 className="position">{data.contentfulInfo.title}</h2>
+    </div>
+    <div className="contact">
+      {data.contentfulInfo.social.map(social => {
+        return (
+          <div className="link" key={social.type}>
+            <Icon type={social.icon} />
+            <a target="_blank" rel="noopener noreferrer" href={social.url}>
+              {social.username}
+            </a>
+          </div>
+        )
+      })}
+    </div>
+    <div className="legal">
+      <p className="credit">
+        Built with{' '}
+        <Icon style={{ color: 'red' }} theme="filled" type="heart" /> using{' '}
+        <a
+          className="mention"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          React.js
+            </a>
+      </p>
+      <p className="copyright">Copyright (c) Mihir Pathak</p>
+    </div>
+  </>
+)
+
+const Layout = ({ children }) => {
   const [showDrawer, toggleDrawer] = useState(false)
-
-  const LinkToRender = () => {
-    if (location === 'home') {
-      return (
-        <div className="contact-wrapper">
-          <Icon type="book" />
-          <Link to="/blog">Read my blog</Link>
-        </div>
-      )
-    }
-    if (location.includes('blog/')) {
-      return (
-        <div className="contact-wrapper">
-          <Icon type="arrow-left" />
-          <Link to="/blog">Go Back</Link>
-        </div>
-      )
-    }
-    return (
-      <div className="contact-wrapper">
-        <Icon type="home" />
-        <Link to="/">Go Home</Link>
-      </div>
-    )
-  }
-
-  const NavbarSvg = () => (
-    <svg
-      viewBox="64 64 896 896"
-      data-icon="menu"
-      width="1em"
-      height="1em"
-      fill="currentColor"
-      aria-hidden="true"
-      focusable="false"
-    >
-      <path d="M904 160H120c-4.4 0-8 3.6-8 8v64c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-64c0-4.4-3.6-8-8-8zm0 624H120c-4.4 0-8 3.6-8 8v64c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-64c0-4.4-3.6-8-8-8zm0-312H120c-4.4 0-8 3.6-8 8v64c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-64c0-4.4-3.6-8-8-8z"></path>
-    </svg>
-  )
 
   const data = useStaticQuery(graphql`
     query {
@@ -71,116 +86,36 @@ const Layout = ({ location, children }) => {
   return (
     <div className="App">
       <div className="sidebar">
-        <Img
-          className="profile-img"
-          fluid={data.contentfulInfo.profileImage.fluid}
-        />
-        <div className="heading">
-          <h1 className="name">{data.contentfulInfo.name}</h1>
-          <h2 className="title">{data.contentfulInfo.title}</h2>
-        </div>
-        <div className="contact">
-          <LinkToRender />
-        </div>
-        <div className="contact">
-          {data.contentfulInfo.social.map(social => {
-            return (
-              <div className="contact-wrapper" key={social.type}>
-                <Icon type={social.icon} />
-                <a target="_blank" rel="noopener noreferrer" href={social.url}>
-                  {social.username}
-                </a>
-              </div>
-            )
-          })}
-        </div>
-        <div className="legal">
-          <p className="using">
-            Built with{' '}
-            <Icon style={{ color: 'red' }} theme="filled" type="heart" /> using{' '}
-            <a
-              className="mention"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              React.js
-            </a>
-          </p>
-          <p className="copyright">Copyright (c) Mihir Pathak</p>
-        </div>
+        {sidebarContent(data)}
       </div>
 
       <div className="topbar">
-        <Link to="/">
-          <Img
-            className="profile-img"
-            fluid={data.contentfulInfo.profileImage.fluid}
-          />
-        </Link>
-        <div className="heading">
-          <h1 className="name">{data.contentfulInfo.name}</h1>
-        </div>
+        <Img
+          className="profile"
+          fluid={data.contentfulInfo.profileImage.fluid}
+        />
+        <span className="title">
+          {data.contentfulInfo.name}
+        </span>
         <Icon component={NavbarSvg} onClick={() => toggleDrawer(true)} />
-
-        <Drawer
-          placement="left"
-          closable={false}
-          onClose={() => toggleDrawer(false)}
-          visible={showDrawer}
-        >
-          <Img
-            className="profile-img"
-            fluid={data.contentfulInfo.profileImage.fluid}
-          />
-          <div className="heading">
-            <h1 className="name">{data.contentfulInfo.name}</h1>
-            <h2 className="title">{data.contentfulInfo.title}</h2>
-          </div>
-          <div className="contact">
-            {data.contentfulInfo.social.map(social => {
-              return (
-                <div className="contact-wrapper" key={social.type}>
-                  <Icon type={social.icon} />
-                  <a
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    href={social.url}
-                  >
-                    {social.username}
-                  </a>
-                </div>
-              )
-            })}
-          </div>
-          <div className="contact">
-            <LinkToRender />
-          </div>
-          <div className="legal">
-            <p className="using">
-              Built with{' '}
-              <Icon style={{ color: 'red' }} theme="filled" type="heart" />{' '}
-              using{' '}
-              <a
-                className="mention"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                React.js
-              </a>
-            </p>
-            <p className="copyright">Copyright (c) Mihir Pathak</p>
-          </div>
-        </Drawer>
       </div>
+
+      <Drawer
+        placement="left"
+        closable={false}
+        width="auto"
+        onClose={() => toggleDrawer(false)}
+        visible={showDrawer}
+      >
+        {sidebarContent(data)}
+      </Drawer>
+
       <div className="container">{children}</div>
     </div>
   )
 }
 
 Layout.propTypes = {
-  location: PropTypes.string.isRequired,
   children: PropTypes.node.isRequired,
 }
 
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,10 +4,7 @@ import { useStaticQuery, graphql } from 'gatsby'
 import Layout from '../components/layout/index'
 import SEO from '../components/seo'
 
-import { Icon, Tabs, Collapse, List, Timeline } from 'antd'
-
-const { TabPane } = Tabs
-const Panel = Collapse.Panel
+import { Timeline } from 'antd'
 
 const IndexPage = () => {
   const data = useStaticQuery(graphql`
@@ -26,26 +23,6 @@ const IndexPage = () => {
           toDate(formatString: "MMM YYYY")
         }
       }
-      allContentfulProjects(sort: { fields: createdAt, order: ASC }) {
-        nodes {
-          url
-          content
-          contentful_id
-          title
-        }
-      }
-      contentfulSkills {
-        skills {
-          Web_Devlopment
-          JavaScript_Frameworks___Libraries
-          PHP_Framweworks
-          CSS_Frameworks___Libraries
-          Database
-          Programming_Languages
-          Cloud___Deployment
-          VCS
-        }
-      }
     }
   `)
 
@@ -53,107 +30,45 @@ const IndexPage = () => {
     <Layout location="home">
       <SEO title="Home" />
       <div className="home">
-        <Tabs>
-          <TabPane tab="Portfolio" key="/">
-            <div className="plural-holder">
-              <p
-                className="about"
-                dangerouslySetInnerHTML={{
-                  __html: data.contentfulInfoAboutTextNode.about,
-                }}
-              ></p>
-              <Timeline>
-                {data.allContentfulExperience.nodes.map(experience => {
-                  return (
-                    <Timeline.Item key={experience.companyUrl}>
-                      <div className="singular-holder" key={experience.title}>
-                        <p className="code-heading">
-                          {experience.title}
-                          {experience.companyName && experience.companyUrl
-                            ? ' @ '
-                            : null}
-                          {experience.companyName && experience.companyUrl ? (
-                            <a
-                              href={experience.companyUrl}
-                              className="mention"
-                              target="_blank"
-                              rel="noreferrer noopener"
-                            >
-                              {experience.companyName}
-                            </a>
-                          ) : null}
-                        </p>
-                        <p className="period">
-                          {experience.fromDate}
-                          {experience.toDate
-                            ? ` - ${experience.toDate}`
-                            : ' - Present'}
-                        </p>
-                        <p className="content">{experience.description}</p>
-                      </div>
-                    </Timeline.Item>
-                  )
-                })}
-              </Timeline>
-            </div>
-          </TabPane>
-          <TabPane tab="Side Projects" key="projects">
-            <div className="plural-holder">
-              {data.allContentfulProjects.nodes.map(project => {
-                return (
-                  <div className="singular-holder" key={project.contentful_id}>
-                    <p className="code-heading">
-                      {project.title}
+        <p
+          className="about"
+          dangerouslySetInnerHTML={{
+            __html: data.contentfulInfoAboutTextNode.about,
+          }}
+        ></p>
+        <Timeline className="timeline">
+          {data.allContentfulExperience.nodes.map(experience => {
+            return (
+              <Timeline.Item key={experience.companyUrl}>
+                <div className="item" key={experience.title}>
+                  <p className="heading">
+                    {experience.title}
+                    {experience.companyName && experience.companyUrl
+                      ? ' @ '
+                      : null}
+                    {experience.companyName && experience.companyUrl ? (
                       <a
+                        href={experience.companyUrl}
+                        className="mention"
                         target="_blank"
-                        rel="noopener noreferrer"
-                        href={project.url}
+                        rel="noreferrer noopener"
                       >
-                        <Icon
-                          type={
-                            project.url.includes('github.com')
-                              ? 'github'
-                              : 'global'
-                          }
-                        />
+                        {experience.companyName}
                       </a>
-                    </p>
-                    <p className="content">{project.content}</p>
-                  </div>
-                )
-              })}
-            </div>
-          </TabPane>
-          <TabPane tab="Skills" key="skills">
-            <div className="plural-holder">
-              <Collapse bordered={false} accordion={true}>
-                {Object.keys(data.contentfulSkills.skills).map((key, index) => {
-                  return (
-                    <Panel
-                      header={key.replace('___', ' & ').replace('_', ' ')}
-                      key={index}
-                    >
-                      <List style={{ marginLeft: '1.4rem' }}>
-                        {data.contentfulSkills.skills[key].map(
-                          (item, index) => {
-                            return (
-                              <List.Item
-                                style={{ padding: '10px 0px' }}
-                                key={index}
-                              >
-                                {item}
-                              </List.Item>
-                            )
-                          }
-                        )}
-                      </List>
-                    </Panel>
-                  )
-                })}
-              </Collapse>
-            </div>
-          </TabPane>
-        </Tabs>
+                    ) : null}
+                  </p>
+                  <p className="period">
+                    {experience.fromDate}
+                    {experience.toDate
+                      ? ` - ${experience.toDate}`
+                      : ' - Present'}
+                  </p>
+                  <p className="content">{experience.description}</p>
+                </div>
+              </Timeline.Item>
+            )
+          })}
+        </Timeline>
       </div>
     </Layout>
   )
